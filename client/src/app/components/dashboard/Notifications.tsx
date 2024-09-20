c884d3d138740a4234bff487935a7e37a0421aa5
aritraroy0 <aritro.roy0@gmail.com>
2024-09-20 17:18:10 -0400

    Client: Added proper dashboard UI

--- client/src/app/components/dashboard/Notifications.tsx
+++ client/src/app/components/dashboard/Notifications.tsx
@@ -3,7 +3,7 @@
 // Notifications.tsx
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { RootState } from '../store';
+import { RootState } from '../../store';
 
 // Define the shape of a notification
 interface Notification {
