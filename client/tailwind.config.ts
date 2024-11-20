2131be9c8a32e9f9f16724c390cec49c484f1d64
root <aritro.roy0@gmail.com>
2024-11-20 04:47:55 -0500

    Client: Caresoul terminal added

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +7,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        paused: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll linear infinite',
+        paused: 'paused 0s linear infinite',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
