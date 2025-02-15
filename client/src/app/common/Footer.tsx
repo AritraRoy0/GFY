7fcc1a742eee22d54461a3765ff46cca8970cf33
aritraroy0 <aritro.roy0@gmail.com>
2025-02-15 00:05:51 -0500

    Updated Footer

--- client/src/app/common/Footer.tsx
+++ client/src/app/common/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer(props: { builtBy: string }) {
         <div className="container mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row">
           <div className="flex flex-col items-center gap-2 px-8 md:flex-row md:gap-4">
             <p className="text-center text-sm md:text-left">
-              © 2025 Vault Tech Technologies. All rights reserved.
+              © 2025 Vault Technologies LLC. All rights reserved.
             </p>
           </div>
           <div className="flex items-center space-x-4">
